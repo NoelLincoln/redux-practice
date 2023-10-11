813b93c7f8d25bc4c62fb34cc98a4fa0b7f3287c
Noel Bryant <noelsobryant@gmail.com>
2023-10-11 19:35:47 +0300

    dispatch calculateTotals every time cartItems changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
+import { useSelector, useDispatch } from 'react-redux';
+import { calculateTotals } from './features/cart/cartSlice';
+
 function App() {
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems, dispatch]);
+
   return (
     <main>
       <Navbar />
@@ -8,4 +18,5 @@ function App() {
     </main>
   );
 }
+
 export default App;
